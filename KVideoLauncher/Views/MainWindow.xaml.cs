8e7dd403960b2fc4f824c33909c1032ae2f7b5df
SuKacaCoding <sukacacoding@163.com>
2023-07-06 18:58:43 +0800

    [Feature] MainWindow: Add key gestures to switch between listboxes.

--- KVideoLauncher/Views/MainWindow.xaml.cs
+++ KVideoLauncher/Views/MainWindow.xaml.cs
@@ -2,11 +2,39 @@
 using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
+using Window = HandyControl.Controls.Window;
 
 namespace KVideoLauncher.Views;
 
 public partial class MainWindow : Window
 {
+    private ListBox FocusedListBox
+    {
+        get => _verticalListBoxes[_focusedListBoxIndex];
+        set
+        {
+            int indexOfValue = FocusOnListBox(value);
+            _focusedListBoxIndex = indexOfValue == -1 ? _focusedListBoxIndex : indexOfValue;
+        }
+    }
+
+    private int FocusedListBoxIndex
+    {
+        get => _focusedListBoxIndex;
+        set
+        {
+            _focusedListBoxIndex = value % _verticalListBoxes.Count;
+            if (_focusedListBoxIndex < 0)
+                _focusedListBoxIndex += _verticalListBoxes.Count;
+            FocusOnListBox(FocusedListBox);
+        }
+    }
+
+    private ICommand MoveListBoxFocusLeftCommand { get; }
+    private ICommand MoveListBoxFocusRightCommand { get; }
+
     private static readonly GridLength SelectedColumnWidth = new(value: 2, GridUnitType.Star);
     private static readonly GridLength GeneralColumnWidth = new(value: 1, GridUnitType.Star);
 
@@ -23,23 +51,54 @@ public partial class MainWindow : Window
                 ColPlaylist
             }
         );
+        _verticalListBoxes = new ReadOnlyCollection<ListBox>
+        (
+            new List<ListBox>
+            {
+                ListDrives,
+                ListDirectories,
+                ListVideos,
+                ListPlaylist
+            }
+        );
 
-        AddHandler(PreviewMouseDownEvent, handler: new RoutedEventHandler(ListBoxMouseDown));
+        FocusedListBoxIndex = 0;
+
+        // Initialize Commands.
+        MoveListBoxFocusLeftCommand = new RelayCommand(MoveListBoxFocusLeft);
+        InputBindings.Add
+        (
+            new KeyBinding
+            {
+                Gesture = new KeyGesture(Key.OemOpenBrackets, ModifierKeys.Control),
+                Command = MoveListBoxFocusLeftCommand
+            }
+        );
+        MoveListBoxFocusRightCommand = new RelayCommand(MoveListBoxFocusRight);
+        InputBindings.Add
+        (
+            new KeyBinding
+            {
+                Gesture = new KeyGesture(Key.OemCloseBrackets, ModifierKeys.Control),
+                Command = MoveListBoxFocusRightCommand
+            }
+        );
     }
 
     private void ListBoxMouseDown(object sender, RoutedEventArgs e)
     {
-        if (e.Source is not ListBox listBox)
-            return;
-        if (listBox == ListPath)
-            return;
-
-        FocusOnListBox(listBox);
+        if (e.Source is ListBox listBox)
+            FocusedListBox = listBox;
     }
 
-    private void FocusOnListBox(ListBox listBox)
+    private int FocusOnListBox(ListBox listBox)
     {
+        int indexOfListBox = _verticalListBoxes.IndexOf(listBox);
+        if (indexOfListBox == -1)
+            return -1;
+
         listBox.Focus();
+
         foreach (var column in _widthAdjustableColumns)
             column.Width = GeneralColumnWidth;
 
@@ -49,7 +108,22 @@ public partial class MainWindow : Window
             ColVideos.Width = SelectedColumnWidth;
         else if (listBox == ListPlaylist)
             ColPlaylist.Width = SelectedColumnWidth;
+
+        return indexOfListBox;
     }
 
+    private void MoveListBoxFocusLeft()
+    {
+        FocusedListBoxIndex--;
+    }
+
+    private void MoveListBoxFocusRight()
+    {
+        FocusedListBoxIndex++;
+    }
+
+    private readonly ReadOnlyCollection<ListBox> _verticalListBoxes;
+
     private readonly ReadOnlyCollection<ColumnDefinition> _widthAdjustableColumns;
+    private int _focusedListBoxIndex;
 }
