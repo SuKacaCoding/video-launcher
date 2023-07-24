478a9167a54459cba0917af8d9ca8a3ef7685dd3
SuKacaCoding <sukacacoding@163.com>
2023-07-24 18:09:32 +0800

    [Feature] EnterDirectoryStrategy: Memorize the last entered path for each drive.

--- KVideoLauncher/Tools/EnterPathStrategies/EnterDirectoryStrategy.cs
+++ KVideoLauncher/Tools/EnterPathStrategies/EnterDirectoryStrategy.cs
@@ -8,10 +8,14 @@ public class EnterDirectoryStrategy : IEnterPathStrategy
 {
     public static EnterDirectoryStrategy Instance => LazyInstance.Value;
     private static readonly Lazy<EnterDirectoryStrategy> LazyInstance = new();
-
+    
     public string Enter(EnterPath enterPath)
     {
         Debug.Assert(condition: enterPath.Path != null, message: "enterPath.Path != null");
-        return Path.GetFullPath(enterPath.Path);
+        string normalizedPath = Path.GetFullPath(enterPath.Path);
+
+        enterPath.LastEnteredPathByDrive[Directory.GetDirectoryRoot(normalizedPath)] = normalizedPath;
+
+        return normalizedPath;
     }
 }
