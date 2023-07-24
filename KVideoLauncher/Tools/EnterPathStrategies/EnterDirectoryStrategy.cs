106382bdf72c042014b1a9ebd4d88504adde65b5
SuKacaCoding <sukacacoding@163.com>
2023-07-24 21:07:40 +0800

    [Format]

--- KVideoLauncher/Tools/EnterPathStrategies/EnterDirectoryStrategy.cs
+++ KVideoLauncher/Tools/EnterPathStrategies/EnterDirectoryStrategy.cs
@@ -8,7 +8,7 @@ public class EnterDirectoryStrategy : IEnterPathStrategy
 {
     public static EnterDirectoryStrategy Instance => LazyInstance.Value;
     private static readonly Lazy<EnterDirectoryStrategy> LazyInstance = new();
-    
+
     public string Enter(EnterPath enterPath)
     {
         Debug.Assert(condition: enterPath.Path != null, message: "enterPath.Path != null");
