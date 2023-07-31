a85d598d236f6fe5cae88815ed1c6e5c2d15cb0b
SuKacaCoding <sukacacoding@163.com>
2023-07-31 14:27:54 +0800

    [Feature] MainWindow.ListFiles: Show the correponding icon for each file.

---
+++ KVideoLauncher/Tools/Converters/FileDisplayingTypeToIconFontStringConverter.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using KVideoLauncher.Data.Enums;
+
+namespace KVideoLauncher.Tools.Converters;
+
+public class FileDisplayingTypeToIconFontStringConverter : IValueConverter
+{
+    public object Convert
+    (
+        object? value, Type targetType, object? parameter,
+        CultureInfo culture
+    )
+    {
+        if (value is not FileDisplayingType displayingType)
+            return string.Empty;
+
+        return displayingType switch
+        {
+            FileDisplayingType.Video => "\ue600",
+            FileDisplayingType.VideoWithSubtitle => "\ue668",
+            FileDisplayingType.Subtitle => "\ue69e",
+            _ => throw new ArgumentOutOfRangeException(nameof(displayingType))
+        };
+    }
+
+    public object ConvertBack
+    (
+        object? value, Type targetType, object? parameter,
+        CultureInfo culture
+    ) =>
+        throw new NotImplementedException();
+}
