7ba3f7474bcbe8d5ee49f21b3bb7c4b4510f2d9d
SuKacaCoding <sukacacoding@163.com>
2023-08-16 20:12:48 +0800

    [Feature] CaseInsensitiveDictionaryJsonConverter: Use it to deserialize into a case-insensitive dictionary.

---
+++ KVideoLauncher/Tools/Converters/CaseInsensitiveDictionaryJsonConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace KVideoLauncher.Tools.Converters;
+
+public class CaseInsensitiveDictionaryJsonConverter<TValue>
+    : JsonConverter<Dictionary<string, TValue>>
+{
+    public override Dictionary<string, TValue> Read
+        (ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var dict = (IDictionary<string, TValue>?)JsonSerializer
+            .Deserialize(ref reader, typeToConvert, options);
+        return dict is { }
+            ? new Dictionary<string, TValue>(dict, StringComparer.OrdinalIgnoreCase)
+            : new Dictionary<string, TValue>(StringComparer.OrdinalIgnoreCase);
+    }
+
+    public override void Write(Utf8JsonWriter writer, Dictionary<string, TValue> value, JsonSerializerOptions options)
+    {
+        JsonSerializer.Serialize
+        (
+            writer, value, inputType: value.GetType(),
+            options
+        );
+    }
+}
